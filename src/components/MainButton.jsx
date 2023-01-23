249bcc52229908df047895236ced14ad4986c64a
August Ollé <august@augustolle.com>
2023-01-23 20:53:45 +0100

    responsive styling

--- src/components/MainButton.jsx
+++ src/components/MainButton.jsx
@@ -9,7 +9,7 @@ function MainButton({ className, buttonNumber, buttonTitle, buttonImg, linkpath,
   return (
     <a className={`${styles.mainCta} ${className} ${theme}`} href={linkpath}>
       {isNew && 
-      <div className={`${styles.newTag}`}>New!</div>
+      <div className={`${styles.newTag} new ${theme}`}>New!</div>
       }
       <img className={`${theme} ${styles.buttonImg} button${buttonNumber}`} src={buttonImg} height='60px' alt={buttonTitle}/>
       <h2 className={styles.title}>{buttonTitle}</h2>
