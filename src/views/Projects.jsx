f65071722dbf44cf665370ce439332c8b6148967
August Ollé <august@augustolle.com>
2023-01-24 20:31:33 +0100

    projects page

--- src/views/Projects.jsx
+++ src/views/Projects.jsx
@@ -3,7 +3,6 @@ import Frame from '../components/Frame'
 import Footer from '../components/Footer'
 import Header from '../components/Header'
 import ProjectsPresentation from '../components/ProjectsPresentation'
-import ProjectsPresentation from '../components/ProjectsPresentation'
 
 function Projects({}) {
 
