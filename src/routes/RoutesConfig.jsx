d42531fc55a0c11158dc3b105b546dbe68c4a5d7
August Ollé <august@augustolle.com>
2023-01-25 11:14:34 +0100

    scroll to top of page

--- src/routes/RoutesConfig.jsx
+++ src/routes/RoutesConfig.jsx
@@ -1,19 +1,21 @@
 import {BrowserRouter as Router, Routes, Route, Link} from 'react-router-dom';
 import Home from '../views/Home';
-import Blog from '../views/Blog';
 import Projects from '../views/Projects';
 import About from '../views/About';
 import Contact from '../views/Contact';
+import ScrollToTop from '../helpers/ScrollToTop';
 
 function RoutesConfig(){
     return(
     <Router>
-        <Link to="/"></Link>
+        {/* <Link to="/"></Link>
         <Link to="/home"></Link>
         <Link to={{pathname: "http://blog.augustolle.com"}}></Link>
         <Link to="/projects"></Link>
         <Link to="/about"></Link>
-        <Link to="/contact"></Link>
+        <Link to="/contact"></Link> */}
+
+    <ScrollToTop />
 
         <Routes>
             <Route path="/" element={ <Home/> } />
