1c18619d3b294130ef0a3b56bbc37067deb5363b
August Ollé <august@augustolle.com>
2023-01-25 10:18:14 +0100

    project cards

--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -1,29 +1,30 @@
-import React from 'react'
+import React, {useContext} from 'react'
 import styles from '../css/projectCard.module.css'
+import {themeContext} from '../contexts/ThemeContext';
 
 
 function ProjectCard({cardTitle, cardText, cardImg, linkpath, github, isNew}) {
 
+    const {theme} = useContext(themeContext);
 
-    
-  return (
-    <div className={`${styles.projectCard}`}>
-        {isNew && 
-        <div className={`${styles.newTag} new ${theme}`}>New!</div>
-        }
-        <div className={`${styles.projectTitle}`}>
-            <h2 className={`${styles.projectName}`}>{cardTitle}</h2>
+    return (
+        <div className={`${styles.projectCard}`}>
+            {isNew && 
+            <div className={`${styles.newTag} new ${theme}`}>New!</div>
+            }
+            <div className={`${styles.projectTitle} title ${theme}`}>
+                <h2 className={`${styles.projectName}`}>{cardTitle}</h2>
+            </div>
+            <div className={`${styles.projectInfo}`}>
+                <p className={`${styles.projectDescription}`}>{cardText}</p>
+                <img  className={`${styles.projectPreview}`} src={cardImg} alt="Project preview" />
+            </div>
+            <div>
+                <a href={linkpath}>Visit project</a>
+                <a href={github}>Check repository in Github</a>
+            </div>
         </div>
-        <div className={`${styles.projectInfo}`}>
-            <p className={`${styles.projectDescription}`}>{cardText}</p>
-            <img  className={`${styles.projectPreview}`} src={cardImg} alt="Project preview" />
-        </div>
-        <div>
-            <a href={linkpath}>Visit project</a>
-            <a href={github}>Check ropository in Github</a>
-        </div>
-    </div>
-  )
+    )
 }
 
 export default ProjectCard
--- src/components/ProjectsPresentation.jsx
+++ src/components/ProjectsPresentation.jsx
@@ -33,9 +33,7 @@ function ProjectsPresentation() {
     <div className={`${styles.presentationContainer} ${theme}`}>
         <div className={`${styles.presentationCard} ${theme} pres`}>
           <h1 className={`${styles.mainTitle}  ${theme} presTitle`}>Projects</h1>
-          {/* <p className={styles.mainTitleEmoji}>🖐️</p> */}
           <div className={`${styles.presentationSubcontainer} ${theme} presText`}>
-            {/* <img src="src/assets/vportrait.png" width="200px" alt="August Ollé profile image" /> */}
             <div className={`${styles.presentationTextWrapper} ${theme}`}>
               <p className={styles.introText}>These are some of the frontend developement projects I have been working on. This list is being updated frequently.<br/>Feel free to navigate each project and review its repository.</p>
             </div>
